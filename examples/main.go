c644d6e73d5ea0ad7c6607208bbe85ca34721d2a
vcaesar <vzvway@gmail.com>
2021-08-02 12:00:47 -0400

    update examples code

--- examples/main.go
+++ examples/main.go
@@ -7,10 +7,20 @@ import (
 )
 
 func main() {
+	findid()
+	run()
+}
+
+func findid() {
 	fpid, err := ps.FindIds("chrome")
 	if err == nil {
 		fmt.Println("pids... ", fpid)
 	} else {
 		fmt.Println(err)
 	}
+}
+
+func run() {
+	o, err := ps.Run("open /Applications/Xcode.app")
+	fmt.Println("run err: ", o, err)
 }
